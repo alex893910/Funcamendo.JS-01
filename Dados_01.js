d4a603ab4027cc5d17c90abbf19eb5eb83ff3076
alex893910 <santosbalbino1983@hotmail.com>
2024-05-20 14:06:19 -0300

    Projetos Js

---
+++ Dados_01.js
@@ -0,0 +1,31 @@
+var x=10; // A palavra var é uma forma antiga de declarar avariavel. Mas existe palavra mais modernas.
+var quantidade=100 
+var decimal=10.075
+var total= quantidade*decimal
+let valor1=200///O let é uma forma mais moderna de se indicar uma variavel. O var é a mais aintiga.
+let  valor2=50
+let valor3=valor1*valor2
+let nome = "Lápes"
+console.log("Ola mundo")
+console.log(x)
+console.log(10)
+console.log(10.02)
+console.log(quantidade)
+console.log(10.023)
+var a=10
+var b=20
+var z=2
+console.log("var a=10")
+console.log("var y=20")
+console.log("z=2")
+
+console.log("a*b=",a*b)
+console.log("(a*b)/z=",(a*b)/z)
+console.log("a*8=",a*8)
+console.log(total)
+console.log("valor1=",valor1)
+console.log("valor2=",valor2)
+console.log("valor3=",valor3)
+console.log(nome)
+/* No VS code, eu posso colocar  ou não o ponto e virgula(;) no 
+fim de cada palavra.*/
